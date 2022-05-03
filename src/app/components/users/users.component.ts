64cb4f61b1de8c028b68cd9258317a00d9af1703
Rodion Sibov <rodionsibov@gmail.com>
2022-05-03 19:55:49 +0300

    update

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getUsers(15).subscribe((results: any) => {
+      console.log(results.results);
+    });
   }
-
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ResponseInterface } from '../interfaces/response-interface';
+import { UserInterface } from '../interfaces/user-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +20,14 @@ export class UserService {
   // fetch one user using the user UUID
   getUser(uuid: number = 1): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`);
+  }
+
+  private processResponse(response: ResponseInterface): ResponseInterface {
+    return {
+      info: {
+        ...response.info,
+      },
+      results: response.results.map((user: any) => (<UserInterface>{})),
+    };
   }
 }
